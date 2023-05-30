28449326e989c4299ad80d66a88d0ecd80ad433e
KiranNayak234 <135014674+KiranNayak234@users.noreply.github.com>
2023-05-30 15:51:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,6 @@ window.addEventListener('DOMContentLoaded', () => {
     let currentPlayer = 'X';
     let isGameActive = true;
 
-    const PLAYERX_WON = 'PLAYERX_WON';
-    const PLAYERO_WON = 'PLAYERO_WON';
-    const TIE = 'TIE';
-
-
-    /*
-        Indexes within the board
-        [0] [1] [2]
-        [3] [4] [5]
-        [6] [7] [8]
-    */
-
     const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -30,6 +18,10 @@ window.addEventListener('DOMContentLoaded', () => {
         [0, 4, 8],
         [2, 4, 6]
     ];
+    
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
 
     function handleResultValidation() {
         let roundWon = false;
